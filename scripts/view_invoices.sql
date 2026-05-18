31ee230e4fe91a7efa396929b77ae7224af67a57
preszka94 <piotrreszka94@gmail.com>
2026-05-18 16:03:38 +0200

    chore: remove SafeDoc duplicates, drop orphan submodule, harden gitignore

--- scripts/view_invoices.sql
+++
@@ -1,8 +0,0 @@
-SET LINESIZE 200;
-COLUMN uuid FORMAT A38;
-COLUMN seller_name FORMAT A40;
-COLUMN total_amount FORMAT 99999.99;
-COLUMN status FORMAT A18;
-
-SELECT uuid, seller_name, total_amount, status FROM invoice_metadata;
-EXIT;
